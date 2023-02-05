f0c83759a348674aa711f13da3146961b8771736
Daniel Hall <daniel.hall@live.se>
2023-02-05 01:53:47 +0100

    Added clamped movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,8 @@ public class PlayerMovement : MonoBehaviour
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
-        playerInput.Normalize();
+        //playerInput.Normalize();
+        playerInput = Vector2.ClampMagnitude(playerInput, 1.0f);
         transform.localPosition = new Vector3(playerInput.x, 0.5f, playerInput.y);
     }
 }
