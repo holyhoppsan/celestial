f4b03e8cad2d87d9f37fa7f4b8d24ed5c0369e40
Daniel Hall <daniel.hall@live.se>
2023-02-05 23:27:33 +0100

    Added bounciness multiplier to the bouncing logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     Rect allowedArea = new Rect(-5.0f, -5.0f, 10.0f, 10.0f);
 
+    [SerializeField, Range(0.0f, 1.0f)]
+    float bounciness = 0.5f;
+
     Vector3 velocity;
 
     // Update is called once per frame
@@ -60,23 +63,23 @@ public class PlayerMovement : MonoBehaviour
             if (newPosition.x < allowedArea.xMin)
             {
                 newPosition.x = allowedArea.xMin;
-                velocity.x = -velocity.x;
+                velocity.x = -velocity.x * bounciness;
             }
             else if (newPosition.x > allowedArea.xMax)
             {
                 newPosition.x = allowedArea.xMax;
-                velocity.x = -velocity.x;
+                velocity.x = -velocity.x * bounciness;
             }
 
             if (newPosition.z < allowedArea.yMin)
             {
                 newPosition.z = allowedArea.yMin;
-                velocity.z = -velocity.z;
+                velocity.z = -velocity.z * bounciness;
             }
             else if (newPosition.z > allowedArea.xMax)
             {
                 newPosition.z = allowedArea.xMax;
-                velocity.z = -velocity.z;
+                velocity.z = -velocity.z * bounciness;
             }
         }
         transform.localPosition = newPosition;
