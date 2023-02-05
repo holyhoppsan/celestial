5bdf03ea80130068042d754630981f4b65582bcb
Daniel Hall <daniel.hall@live.se>
2023-02-05 01:52:25 +0100

    Added normalization based movement' '

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
+        playerInput.Normalize();
         transform.localPosition = new Vector3(playerInput.x, 0.5f, playerInput.y);
     }
 }
