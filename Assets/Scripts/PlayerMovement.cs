176d4d532b928427ce9f12bfded4a54cc8f3c2eb
Daniel Hall <daniel.hall@live.se>
2023-02-05 22:18:33 +0100

    Added movement with clamped desired max velocity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField, Range(0.0f, 100.0f)]
     float maxSpeed = 10.0f;
 
+    [SerializeField, Range(0.0f, 100.0f)]
+    float maxAcceleration = 10.0f;
+
     Vector3 velocity;
 
     // Update is called once per frame
@@ -17,9 +20,31 @@ public class PlayerMovement : MonoBehaviour
         playerInput.y = Input.GetAxis("Vertical");
         //playerInput.Normalize();
         //playerInput = Vector2.ClampMagnitude(playerInput, 1.0f);
-        Vector3 acceleration = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
-        velocity += acceleration * Time.deltaTime;
+        // Acceleration based movement
+        // Vector3 acceleration = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+
+        if (velocity.x < desiredVelocity.x)
+        {
+            velocity.x = Mathf.Min(velocity.x + maxSpeedChange, desiredVelocity.x);
+        }
+        else if (velocity.x > desiredVelocity.x)
+        {
+            velocity.x = Mathf.Max(velocity.x - maxSpeedChange, desiredVelocity.x);
+        }
+
+        if (velocity.z < desiredVelocity.z)
+        {
+            velocity.z = Mathf.Min(velocity.z + maxSpeedChange, desiredVelocity.z);
+        }
+        else if (velocity.z > desiredVelocity.z)
+        {
+            velocity.z = Mathf.Max(velocity.z - maxSpeedChange, desiredVelocity.z);
+        }
+
         Vector3 displacement = velocity * Time.deltaTime;
+
         transform.localPosition += displacement;
     }
 }
